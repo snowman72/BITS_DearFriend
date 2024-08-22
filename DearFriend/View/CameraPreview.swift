222471c8b9096b004f4a66661346340074e97385
Vu Minh Ha <s3978681@rmit.edu.vn>
2024-08-22 14:31:36 +0700

    Create CameraPreview.swift

---
+++ DearFriend/View/CameraPreview.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreview.swift
+//  DearFriend
+//
+//  Created by Vũ Minh Hà on 22/8/24.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct CameraPreview: UIViewRepresentable {
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
